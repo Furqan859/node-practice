f523a36261d64724d06aa535a3e05013772c70e7
Furqan-Rasool <furqanrasool2@gmail.com>
2022-08-12 09:57:02 +0500

    write file creates a multiple files usin loop

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const fs = require('fs');
-const input = process.argv;
-
-if(input[2] == 'add'){
-fs.writeFileSync(input[3],input[4])
-}else if(input[2] == 'remove'){
-    fs.unlinkSync(input[3]);
-}else{
-    console.log("invalid input")
+const path = require('path');
+const dirName = path.join(__dirname, 'files')
+for(let i = 0; i <5;i++) {
+fs.writeFileSync(dirName + "/hello"+i+".txt","this is a file")
 }
