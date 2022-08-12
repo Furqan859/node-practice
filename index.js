3d33c38c7d0f532d0b3ffc2a55f4e0bd8f0776cd
Furqan-Rasool <furqanrasool2@gmail.com>
2022-08-12 09:21:40 +0500

    make api

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const http = require('http');
+const data = require('./data');
 
 http.createServer((req,res) => {
 res.writeHead(200,{'Content-Type': 'application/json'});
-res.write(JSON.stringify({name:"furqan",email:"furqan@gmail.com",contact:"03055673859"}));
+res.write(JSON.stringify(data)); 
 res.end();
 }).listen(8000)
