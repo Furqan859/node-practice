edf87372cf85e5f8868703a95de0234389c6f739
Furqan-Rasool <furqanrasool2@gmail.com>
2022-08-12 08:43:58 +0500

    add packages and check how to integrate an run in node js.

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-const http = require('http');
+const colors = require('colors');
+// const chalk = require('chalk');
+console.log("package hello".yellow)
+
+// const log = console.log;
+// log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+
+var http = require('http');
 
 http.createServer((req, res) => {
-    res.write("hello world");
-    res.end;
-}).listen(4500);
+    // res.writeHead(200, {'content-Type': 'text/plain'});
+    res.write("<h1>hello world this is world</h1>");
+    res.end();
+}).listen(8080);
+
+
+
+// var http = require('http');
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.write('Hello World!');
+//   res.end();
+// }).listen(8080);
