67fccdbfe113f8f21c3f3b025a5780feb758a7d5
Furqan-Rasool <furqanrasool2@gmail.com>
2022-08-12 18:17:38 +0500

    write file and save on path

--- index.js
+++ index.js
@@ -1,14 +1,50 @@
 const fs = require('fs');
 const path = require('path');
-const dirName = path.join(__dirname, 'files');
+const dirName = path.join(__dirname, 'crud');
+const filePath = `${dirName}/furqan.txt`;
+fs.writeFileSync(filePath,"this is sample file")
 
-for(let i = 0; i <5;i++) {
-    fs.writeFileSync(dirName+"/Hello"+i+".txt","This is 2nd file")
-}
 
-fs.readdir(dirName,(err,file) => {
-    file.forEach((item)=>{
-     console.log(item)
-    })
-    console.log(file[0])
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fs = require('fs');
+// const path = require('path');
+// const dirPath = path.join(__dirname, 'crud');
+// const filepath = `${dirPath}/apple.txt`;
+// fs.writeFileSync(filepath,"this is simple file")
+
+
+
+
+
+
+
+
+
+// const fs = require('fs');
+// const path = require('path');
+// const dirName = path.join(__dirname, 'files');
+
+// for(let i = 0; i <5;i++) {
+//     fs.writeFileSync(dirName+"/Hello"+i+".txt","This is 2nd file")
+// }
+
+// fs.readdir(dirName,(err,file) => {
+//     file.forEach((item)=>{
+//      console.log(item)
+//     })
+//     console.log(file[0])
+// })
