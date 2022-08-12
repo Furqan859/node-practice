9f580e506f2e5241af802a009ea681b5bf83980a
Furqan-Rasool <furqanrasool2@gmail.com>
2022-08-12 08:49:17 +0500

    i will create a first api

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
-const colors = require('colors');
-// const chalk = require('chalk');
-console.log("package hello".yellow)
+const http = require('http');
 
-// const log = console.log;
-// log(chalk.blue('Hello') + ' World' + chalk.red('!'));
-
-var http = require('http');
-
-http.createServer((req, res) => {
-    // res.writeHead(200, {'content-Type': 'text/plain'});
-    res.write("<h1>hello world this is world</h1>");
-    res.end();
-}).listen(8080);
-
-
-
-// var http = require('http');
-// http.createServer(function (req, res) {
-//   res.writeHead(200, {'Content-Type': 'text/plain'});
-//   res.write('Hello World!');
-//   res.end();
-// }).listen(8080);
+http.createServer((req,res) => {
+res.writeHead(200,{'Content-Type': 'application/json'});
+res.write(JSON.stringify({name:"furqan",email:"furqan@gmail.com",contact:"03055673859"}));
+res.end();
+}).listen(8000)
