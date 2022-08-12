7f26b8f6bee080e6509332f73ddefb3fd1109f63
Furqan-Rasool <furqanrasool2@gmail.com>
2022-08-12 10:08:28 +0500

    how can add files and using foreach to display single files and use readdir

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const fs = require('fs');
 const path = require('path');
-const dirName = path.join(__dirname, 'files')
+const dirName = path.join(__dirname, 'files');
+
 for(let i = 0; i <5;i++) {
-fs.writeFileSync(dirName + "/hello"+i+".txt","this is a file")
+    fs.writeFileSync(dirName+"/Hello"+i+".txt","This is 2nd file")
 }
+
+fs.readdir(dirName,(err,file) => {
+    file.forEach((item)=>{
+     console.log(item)
+    })
+    console.log(file[0])
+})
