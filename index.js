b79f7e7aafb1e8bdba72f6422f6c8669ffec2421
Furqan-Rasool <furqanrasool2@gmail.com>
2022-08-13 09:12:36 +0500

    make simple page practise and ass 404 page not found

--- index.js
+++ index.js
@@ -3,8 +3,23 @@ const path = require('path')
 
 const app = express()
 const dirpath = path.join(__dirname, 'public');
+// app.use(express.static(dirpath));
 
-app.use(express.static(dirpath));
+app.get('', (req, res) =>{
+    res.sendFile(`${dirpath}/index.html`)
+})
+
+app.get('/about', (req, res) =>{
+    res.sendFile(`${dirpath}/about.html`)
+})
+
+app.get('/help', (req, res) =>{
+    res.sendFile(`${dirpath}/help.html`)
+})
+
+app.get('*', (req, res) =>{
+    res.sendFile(`${dirpath}/nopage.html`)
+})
 
 app.listen(5000);
 
