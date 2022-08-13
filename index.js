c1bc11f01418e5a8f2d13529dbf06785218fb929
Furqan-Rasool <furqanrasool2@gmail.com>
2022-08-13 09:08:14 +0500

    how run page html with routes

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const path = require('path')
 
-app.get('',(req, res)=>{
-    
-    res.send(`<h1>this is home page</h1>
-    <a href="/about">About</a>
-    <a href="/help">help</a>
-    `)
-})
+const app = express()
+const dirpath = path.join(__dirname, 'public');
 
-app.get('/about',(req, res)=>{
-    res.send(`<input type="text placeholder="username value="${req.query.name}" />
-    <button>My Button</button>
-    <a href="/">home</a>
-    `)
-})
-
-app.get('/help',(req,res)=>{
-    res.send(  `Hello this is help page
-    <a href="/">home</a>
-    `
-    )
-})
-
+app.use(express.static(dirpath));
 
 app.listen(5000);
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require('express');
+// const app = express();
+
+// app.get('',(req, res)=>{
+    
+//     res.send(`<h1>this is home page</h1>
+//     <a href="/about">About</a>
+//     <a href="/help">help</a>
+//     `)
+// })
+
+// app.get('/about',(req, res)=>{
+//     res.send(`<input type="text placeholder="username value="${req.query.name}" />
+//     <button>My Button</button>
+//     <a href="/">home</a>
+//     `)
+// })
+
+// app.get('/help',(req,res)=>{
+//     res.send(  `Hello this is help page
+//     <a href="/">home</a>
+//     `
+//     )
+// })
+
+
+// app.listen(5000);
