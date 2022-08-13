730cf6d71514d438aaf9f89f82dff5bd36a0fa54
Furqan-Rasool <furqanrasool2@gmail.com>
2022-08-14 04:44:33 +0500

    coneect database

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config()
 const app = express();
 
 const reqFile = require('./middleware');
@@ -15,10 +17,18 @@ app.get('/about',(req,res)=>{
     res.send("this is a about page")
 })
 
-route.get('/help',(req,res)=>{
+app.get('/help',reqFile,(req,res)=>{
     res.send("this is a help page")
 })
 
 app.use('/',route);
 
-app.listen(5000);
+const PORT = process.env.PORT  || 3000;
+
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`server is runing on ${PORT}`);
+    });
+}).catch((err)=>{
+    console.log("error",err)    
+})
