9a63cda5a7699319388de3788f767c3b80948b44
Furqan-Rasool <furqanrasool2@gmail.com>
2022-08-22 15:19:16 +0500

    how can find data from database

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 const {MongoClient} = require('mongodb');
-const express = require('express');
-const app = express();
 require('dotenv').config()
-
+// const url = require('mongodb://localhost:27017');
+const client = new MongoClient(process.env.URI);
 const database = 'admin';
-const client = new MongoClient(process.env.URI)
 
-async function getData() {
-    let result = await client.connect();
-    let db = result.db(database);
-    let collection = db.collection('data');
-    let response = await collection.find({}).toArray();
-    console.log(response)
+async function getData(){
+ const result = await client.connect();
+ const db = result.db(database);
+ const collection = db.collection('data');
+ let response = await collection.find({name:"furqan"}).toArray();
+ console.log(response);
 
 }
 
 getData();
-app.listen(process.env.PORT)
-
-
