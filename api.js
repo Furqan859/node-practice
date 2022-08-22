8bd15b6a1a27691500ca7c25e50b447cf45c821a
Furqan-Rasool <furqanrasool2@gmail.com>
2022-08-22 19:41:44 +0500

    connect mongo and save data in mongo from postman apis

--- api.js
+++ api.js
@@ -1,13 +1,23 @@
-const dbConnect = require('./database');
-const express = require('express');
-require('dotenv').config();
+const dbConnect = require('./database')
+const express = require('express')
 const app = express();
+require('dotenv').config();
 
 
-app.get('/',async (req,res)=>{
-  let data = await dbConnect();
-  data = await data.find().toArray();
-  res.send(data);
-});
+app.use(express.json());
+
+app.get('/', async (req, res)=>{
+    let data = await dbConnect();
+     data = await data.find().toArray()
+     res.send(data);
+})
+
+
+app.post('/',async (req, res)=>{
+    let data = await dbConnect();
+    let result = await data.insert(req.body);
+    res.send(result);
+})
+
 
 app.listen(process.env.PORT)
