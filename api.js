7450d63e5c6f84b5b436202d5100e1029180f9bc
Furqan-Rasool <furqanrasool2@gmail.com>
2022-08-23 06:03:29 +0500

    crued operatins in node

--- api.js
+++ api.js
@@ -1,21 +1,40 @@
 const dbConnect = require('./database')
 const express = require('express')
+const mongodb = require('mongodb')
 const app = express();
 require('dotenv').config();
 
 
 app.use(express.json());
 
-app.get('/', async (req, res)=>{
+app.get('/', async (req, res) => {
     let data = await dbConnect();
-     data = await data.find().toArray()
-     res.send(data);
+    data = await data.find().toArray()
+    res.send(data);
 })
 
 
-app.post('/',async (req, res)=>{
+app.post('/', async (req, res) => {
     let data = await dbConnect();
     let result = await data.insert(req.body);
+    res.send(result);
+})
+
+
+app.put('/:name', async (req, res) => {
+    let data = await dbConnect();
+    let result = await data.updateOne(
+        { name: req.params.name },
+        { $set: req.body }
+    );
+    res.send(result);
+})
+
+
+app.delete('/:id', async (req, res) => {
+    let data = await dbConnect();
+    let result = await data.deleteOne({ _id: new mongodb.ObjectId(req.params.id) })
+    console.log(req.params.id);
     res.send(result);
 })
 
