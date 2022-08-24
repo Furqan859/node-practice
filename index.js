37bfc2a85e747364e5006bc3e09324ebc9720959
Furqan-Rasool <furqanrasool2@gmail.com>
2022-08-24 06:47:05 +0500

    mongoose crud delete and update find and save operation on routing with apis

--- index.js
+++ index.js
@@ -20,4 +20,23 @@ app.get('/list',async (req, res) => {
     res.send(data);
 })
 
+app.delete('/delete/:_id',async (req, res) => {
+  
+    let data = await product.deleteOne(req.params);
+    console.log(req.params)
+    res.send(data)
+})
+
+
+
+app.put('/update/:_id',async (req, res) => {
+  
+    let data = await product.updateOne(
+        req.params,
+        {$set: req.body}
+        );
+    console.log(req.params)
+    res.send(data)
+})
+
 app.listen(5000); 
