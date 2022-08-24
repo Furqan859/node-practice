fc07c793eeb29574d194354283a5c741f40cd82a
Furqan-Rasool <furqanrasool2@gmail.com>
2022-08-24 08:14:04 +0500

    file-upload

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 const express = require('express');
 require('./config');
 const product = require('./product');
+const multer = require('multer');
 
 const app = express();
 
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, "uploads")
+        },
+        filename: function (req, file, cb) {
+            cb(null, file.fieldname + "-" + Date.now() + ".jpg")
+        }
+    })
+}).single("user_file")
+
 app.use(express.json());
 
-app.get('/search/:key', async (req, res)=>{
-    console.log(req.params.key)
-
-    let data = await product.find(
-        {
-            "$or":[
-                {"name":{$regex:req.params.key}},
-                {"category":{$regex:req.params.key}},
-                {"age":{$regex:req.params.key}}
-            ]
-        }
-    )
-    res.send(data)
+app.post('/upload', upload, (req, res) => {
+    res.send("file uploaded");
 })
+
 
 app.listen(5000); 
